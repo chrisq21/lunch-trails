8a728c852133ccb367bd4678aefc1f077aa91f6f
Chris Queen <chrisqueen10@gmail.com>
2022-05-31 23:04:50 -0400

    Updated Readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `AllTrails at Lunch`,
-    description: `AllTrails coding assessment`,
+    description: ``,
   },
   plugins: [
     "gatsby-plugin-emotion",
