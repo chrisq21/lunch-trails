1c5f16a1abfcc70edc6d1d52bd36b22894e3155f
Chris Queen <chrisqueen10@gmail.com>
2022-05-31 23:37:19 -0400

    Delete style.ts

--- src/components/RestaurantList/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled"
-import { backgroundGray } from "../../styles/colors"
-
-export const ListContainer = styled.div`
-  grid-area: list;
-  background: ${backgroundGray};
-  padding: 2rem;
-  overflow-y: scroll;
-`
