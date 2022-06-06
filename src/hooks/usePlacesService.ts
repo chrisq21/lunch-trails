d9236a3284178339ec4bad5dc4df1addc11aa251
Chris Queen <chrisqueen10@gmail.com>
2022-06-06 14:01:21 -0400

    Added details api call in usePlacesService hook

--- src/hooks/usePlacesService.ts
+++ src/hooks/usePlacesService.ts
@@ -20,9 +20,40 @@ const useNearbySearchService = (map, service, searchQuery = "") => {
     if (map) {
       const getPlaces = () => {
         try {
-          service.nearbySearch(requestOptions, (places, status) => {
+          let detailsPromises = []
+          service.nearbySearch(requestOptions, async (places, status) => {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
-              setPlaces(places)
+              // Use the details api in order to access a place's website.
+              // Only access details for the first 9 places so that we don't go over the details api quota.
+              const placesSubarray = places.slice(0, 9)
+              placesSubarray.forEach(place => {
+                const detailsRequest = {
+                  placeId: place.place_id,
+                  fields: ["website"],
+                }
+
+                const detailsPromise = new Promise((resolve, reject) => {
+                  service.getDetails(detailsRequest, (placeDetails, status) => {
+                    if (
+                      status == google.maps.places.PlacesServiceStatus.OK &&
+                      placeDetails?.website
+                    ) {
+                      resolve({ ...place, ...placeDetails })
+                    } else {
+                      reject("No details found")
+                    }
+                  })
+                })
+                detailsPromises.push(detailsPromise)
+              })
+
+              try {
+                const placesWithDetails = await Promise.all(detailsPromises)
+                console.log(placesWithDetails)
+                setPlaces(placesWithDetails)
+              } catch (error) {
+                console.log(error)
+              }
             }
           })
         } catch (error) {
